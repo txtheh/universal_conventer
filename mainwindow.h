9325aab032db674b49183d3ee04bbaccb8e7978b
spidermanchik1 <spider-manchik1@yandex.ru>
2026-05-21 10:24:09 +0300

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -9,15 +9,23 @@ class MainWindow;
 }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow() override;
 
+private slots:
+    void onConvertClicked();
+    void onLoadFileClicked();
+    void onSaveFileClicked();
+
 private:
     Ui::MainWindow *ui;
+
+    QString lastOutput;
+    bool    lastTruncated = false;
 };
+
 #endif // MAINWINDOW_H
